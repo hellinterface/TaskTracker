6ae026d37cd0af94e7ed94e5d566fd786a3d2dad
hellinterface <121038717+hellinterface@users.noreply.github.com>
2023-05-19 19:01:28 +1000

    Update TaskListItem.xaml.cs

--- TaskListItem.xaml.cs
+++ TaskListItem.xaml.cs
@@ -15,15 +15,15 @@ using System.Windows.Shapes;
 
 namespace TaskTracker
 {
-    /// <summary>
-    /// Interaction logic for TaskListItem.xaml
-    /// </summary>
+    //
+    // Кастомный элемент задачи (на странице просмотра карточки)
+    //
     public partial class TaskListItem : UserControl//, ICheckElement
     {
         private OBJ_Board Board;
         private OBJ_Card Card;
         public OBJ_Task TaskObject { get; }
-        private CardDetailsPage ParentPage;
+        private CardDetailsPage ParentPage; // Родительская страница
 
         public TaskListItem(OBJ_Board board, OBJ_Card card, OBJ_Task taskObject, CardDetailsPage parentPage)
         {
@@ -33,44 +33,48 @@ namespace TaskTracker
             TaskObject = taskObject;
             ParentPage = parentPage;
             this.DataContext = taskObject;
-            UpdateAppearance();
+            UpdateAppearance(); // Обновить внешний вид
         }
 
+        // Обновление внешнего вида
         private void UpdateAppearance()
         {
-            if (TaskObject.Done == true)
+            if (TaskObject.Done == true) // Отмечено как сделанное
             {
                 Button_CheckBox.Style = (Style)FindResource("ButtonStyle_Main");
             }
-            else
+            else // Не сделанное
             {
                 Button_CheckBox.Style = (Style)FindResource("ButtonStyle_Inactive");
             }
         }
 
+        // Нажатие на кнопку сделано/не сделано
         private void Button_CheckBox_Click(object sender, RoutedEventArgs e)
         {
-            TaskObject.Done = !TaskObject.Done;
-            UpdateAppearance();
-            DatabaseCommunicator.UPDATE_Task(Board, TaskObject);
+            TaskObject.Done = !TaskObject.Done; // Поменять отметку в объекте задачи
+            UpdateAppearance(); // Обновить внешний вид
+            DatabaseCommunicator.UPDATE_Task(Board, TaskObject); // Запрос в БД
         }
 
+        // Нажатие на кнопку удаления задачи
         private void Button_Delete_Click(object sender, RoutedEventArgs e)
         {
-            bool success = DatabaseCommunicator.DEL_Task(Board, Card, TaskObject);
+            bool success = DatabaseCommunicator.DEL_Task(Board, Card, TaskObject); // Запрос в БД
             if (success == true)
             {
                 ParentPage.OnTaskDelete(this);
             }
         }
 
+        // Событие потери фокуса на поле ввода текста задачи
         private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            // Не используем байндинг TwoWay, потому что это событие срабатывает до обновления свойства Title.
+            // Не используем байндинг TwoWay, потому что это событие срабатывает до обновления свойства Text.
             if (TaskObject.Text != TextBox_Text.Text)
             {
-                TaskObject.Text = TextBox_Text.Text;
-                DatabaseCommunicator.UPDATE_Task(Board, TaskObject);
+                TaskObject.Text = TextBox_Text.Text; // Обновление свойства в объекте задачи
+                DatabaseCommunicator.UPDATE_Task(Board, TaskObject); // Запрос в БД
             }
         }
     }
