9e64345b9c804dcbaa56cc5761ace3e7a9cbb982
hellinterface <121038717+hellinterface@users.noreply.github.com>
2023-05-19 18:57:10 +1000

    Update RegisterPage.xaml.cs

--- RegisterPage.xaml.cs
+++ RegisterPage.xaml.cs
@@ -15,9 +15,9 @@ using System.Windows.Shapes;
 
 namespace TaskTracker
 {
-    /// <summary>
-    /// Interaction logic for RegisterPage.xaml
-    /// </summary>
+    //
+    // Страница регистрации
+    //
     public partial class RegisterPage : Page
     {
         public RegisterPage()
@@ -25,38 +25,42 @@ namespace TaskTracker
             InitializeComponent();
         }
 
+        // Нажатие кнопки ОК
         private void Button_OK_Click(object sender, RoutedEventArgs e)
         {
             if (TextBox_Username.Text.Length < 2 || !TextBox_Username.Text.All(char.IsLetterOrDigit))
             {
+                // Неверное имя пользователя
                 TextBox_Username.Style = (Style)FindResource("TextBoxStyle_Incorrect");
+                return;
             }
             if (TextBox_Password.Text.Length < 3 || Utilities.ContainsBannedCharacters(TextBox_Password.Text))
             {
+                // Неверный пароль
                 TextBox_Password.Style = (Style)FindResource("TextBoxStyle_Incorrect");
+                return;
             }
+            // Создание объекта
             OBJ_User user = new OBJ_User()
             {
                 Username = TextBox_Username.Text,
                 Password = TextBox_Password.Text
             };
-            bool success = DatabaseCommunicator.ADD_User(user);
+            bool success = DatabaseCommunicator.ADD_User(user); // Запрос в БД
             if (success)
             {
                 Application.Current.Properties["CurrentUser"] = user;
-                NavigationService.Navigate(new HomePage());
+                NavigationService.Navigate(new HomePage()); // Переход на домашнюю страницу
             }
             else
             {
+                // Неудача
                 TextBox_Username.Style = (Style)FindResource("TextBoxStyle_Incorrect");
                 TextBox_Password.Style = (Style)FindResource("TextBoxStyle_Incorrect");
             }
-            // отправка на сервер
-            // проверка успешности
-            // подсвечение полей красным если что-то не так
-            // если ок то на домашнюю страницу
         }
-
+        
+        // Нажатие кнопки назад (на страницу входа)
         private void Button_ToLogin_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new LoginPage());
